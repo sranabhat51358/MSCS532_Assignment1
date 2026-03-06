904aeff0d913561711f96c06606580b32875a0d3
sranabhat51358 <sranabhat51358@ucumberlands.edu>
2026-03-06 15:49:30 -0500

    added comment and added readme file

---
+++ README.md
@@ -0,0 +1,39 @@
+# MSCS532 – Assignment 1: Insertion Sort
+
+## Objective
+Sort a user-provided list of numbers in **monotonically decreasing order** using the **Insertion Sort algorithm**.
+
+## Python Version
+- Python 3.8 or higher
+
+## How to Run?
+
+### Clone the project with following command
+```bash
+git clone https://github.com/sranabhat51358/MSCS532_Assignment1.git
+cd MSCS532_Assignment1
+```
+### Run the python file using following command
+```bash
+python insertion_sort.py
+```
+### Follow the prompts to provide input:
+1. Please enter the number of elements you want to sort:
+2. Enter element {i+1}:
+
+## Input Example
+```
+Please enter the number of elements you want to sort: 6
+Enter element 1: 13
+Enter element 2: 17
+Enter element 3: 9
+Enter element 4: 4
+Enter element 5: 64
+Enter element 6: 24
+```
+
+## Output Example
+```
+Provided list for sorting:  [13, 17, 9, 4, 64, 24]
+Sorted List in Monotonically Decreasing Order:  [64, 24, 17, 13, 9, 4]
+```
--- insertion_sort.py
+++ insertion_sort.py
@@ -1,42 +1,39 @@
 def insertion_sort_desc(arr):
+    """
+    This program is used to sort a user provided list of elements in monotonically
+    decreasing order using insertion sort algorithm.
+    Args:
+        arr- array to sort
+    Returns:
+        arr- sorted array in decreasing order
+    """
     for i in range(1, len(arr)):
         key = arr[i]
-        
-        print("i:",i)
-        print("Key:", key)
-        
         j = i - 1
-        
-        print("J", j)
-        
+                
         while j >= 0 and arr[j] < key:
             arr[j + 1] = arr[j]
             j -= 1
             
-        print("arr[j]:",arr[j])
-        print("arr[j + 1]:",arr[j + 1])
-        print("Array:", arr)
-
         arr[j + 1] = key
         
-        print("arr[j + 1] after replacing:", arr[j + 1])
-        print("Array aftter replacing:", arr)
-
-    print("Length of List:", len(arr))
-
     return arr
 
 if __name__ == "__main__":
+    # Ask user for the length of array
     count = int(input("Please enter the number of elements you want to sort: "))
 
+    #Initialize an empty list
     user_list = []
 
+    # Ask user to input value till the length of array
     for i in range(count):
         value = int(input(f"Enter element {i+1}: "))
         user_list.append(value)
 
     print("Provided list for sorting: ", user_list)
 
+    #sort the list in decreasing order
     output = insertion_sort_desc(user_list)
     print("Sorted List in Monotonically Decreasing Order: ",output)
 
