09b1f21ab51cdc046392da261b43159f17b327f2
sranabhat51358 <sranabhat51358@ucumberlands.edu>
2026-03-06 14:21:18 -0500

    printed the value to trace during exection

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,17 +1,33 @@
 def insertion_sort_desc(arr):
     for i in range(1, len(arr)):
         key = arr[i]
+        
+        print("i:",i)
+        print("Key:", key)
+        
         j = i - 1
-
+        
+        print("J", j)
+        
         while j >= 0 and arr[j] < key:
             arr[j + 1] = arr[j]
             j -= 1
+            
+        print("arr[j]:",arr[j])
+        print("arr[j + 1]:",arr[j + 1])
+        print("Array:", arr)
 
         arr[j + 1] = key
+        
+        print("arr[j + 1] after replacing:", arr[j + 1])
+        print("Array aftter replacing:", arr)
+
+    print("Length of List:", len(arr))
 
     return arr
 
 if __name__ == "__main__":
     array = [2,3,1,8,3,7,4,10,0]
+    print("Provided List:", array)
     output = insertion_sort_desc(array)
-    print(output)
+    print("Sorted List in Monotonically Decreasing Order:",output)
